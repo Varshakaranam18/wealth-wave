973241ef64d4dd76a339976e9f812acd97faaba2
Varshakaranam18 <varshakaranam18@gmail.com>
2024-06-10 11:56:07 -0400

    initial commit

---
+++ client/src/theme.ts
@@ -0,0 +1,106 @@
+export const tokens = {
+  grey: {
+    100: "#f0f0f3",
+    200: "#e1e2e7",
+    300: "#d1d3da",
+    400: "#c2c5ce",
+    500: "#b3b6c2",
+    600: "#8f929b",
+    700: "#6b6d74",
+    800: "#48494e",
+    900: "#242427",
+  },
+  primary: {
+    // purple
+    100: "#f3e6ff",
+    200: "#e6ccff",
+    300: "#d9b3ff",
+    400: "#cc99ff",
+    500: "#bf80ff",
+    600: "#9966cc",
+    700: "#734d99",
+    800: "#4d3366",
+    900: "#261a33",
+  },
+  secondary: {
+    // white
+    100: "#ffffff",
+    200: "#f2f2f2",
+    300: "#e6e6e6",
+    400: "#d9d9d9",
+    500: "#cccccc",
+    600: "#b3b3b3",
+    700: "#999999",
+    800: "#808080",
+    900: "#666666",
+  },
+  tertiary: {
+    // purple
+    500: "#8884d8",
+  },
+  background: {
+    light: "#2d2d34",
+    main: "#1f2026",
+  },
+};
+
+// mui theme settings
+export const themeSettings = {
+  palette: {
+    primary: {
+      ...tokens.primary,
+      main: tokens.primary[500],
+      light: tokens.primary[400],
+    },
+    secondary: {
+      ...tokens.secondary,
+      main: tokens.secondary[100],
+    },
+    tertiary: {
+      ...tokens.tertiary,
+    },
+    grey: {
+      ...tokens.grey,
+      main: tokens.grey[500],
+    },
+    background: {
+      default: tokens.background.main,
+      light: tokens.background.light,
+    },
+  },
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+    fontSize: 12,
+    h1: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 32,
+    },
+    h2: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 24,
+    },
+    h3: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 20,
+      fontWeight: 800,
+      color: tokens.grey[200],
+    },
+    h4: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 14,
+      fontWeight: 600,
+      color: tokens.grey[300],
+    },
+    h5: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 12,
+      fontWeight: 400,
+      color: tokens.grey[500],
+    },
+    h6: {
+      fontFamily: ["Inter", "sans-serif"].join(","),
+      fontSize: 10,
+      color: tokens.grey[700],
+    },
+  },
+};
