8a4f0442cf75678c72580d90fbdf0ae52533a544
Varshakaranam18 <varshakaranam18@gmail.com>
2024-06-10 17:58:58 -0400

    color

--- client/src/expanded-theme.ts
+++ client/src/expanded-theme.ts
@@ -1,7 +1,4 @@
 // Import the required types from MUI
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
-
 // Declare module augmentation
 declare module "@mui/material/styles/createPalette" {
   // Extend the PaletteColor interface to include an index signature
